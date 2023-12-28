6ce6bf2b10d72bfa1824c43a71ab2d8f0da59442
Matheus Davi <mahteus2008@gmail.com>
2023-12-28 17:03:16 -0300

    Configurando o caminho do build

--- src/server.js
+++ src/server.js
@@ -1,13 +1,19 @@
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('build'));
+// Atualize esta linha para usar 'dist' em vez de 'build'
+app.use(express.static(path.resolve(__dirname, '..', 'dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve('build', 'index.html'));
+  // Atualize esta linha também
+  res.sendFile(path.resolve(__dirname, '..', 'dist', 'index.html'));
 });
 
 app.listen(port, () => {
